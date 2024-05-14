038e9e67b56bab53857186ee1e1221a5a711e2a9
mugishab2020 <mugishab2020@gmail.com>
2024-05-14 15:10:17 +0200

    We have started this

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     '''this is the index page'''
-    return render_template('/templates/0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
